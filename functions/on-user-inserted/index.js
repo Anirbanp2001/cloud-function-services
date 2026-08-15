1191d54ec52338ffc4fed9aa371885a3ecd1b9ec
Anirban Paul <anirbanpaul@Anirbans-MacBook-Air.local>
2026-08-15 21:58:05 +0530

    Changes

--- functions/on-user-inserted/index.js
+++ functions/on-user-inserted/index.js
@@ -3,7 +3,7 @@ const brevo = require('@getbrevo/brevo')
 const admin = require('firebase-admin')
 
 admin.initializeApp()
-console.log('firebase-admin version:', require('firebase-admin/package.json').version);
+// console.log('firebase-admin version:', require('firebase-admin/package.json').version);
 console.log('admin keys:', Object.keys(admin));
 console.log('typeof admin.auth:', typeof admin.auth);
 // console.log(brevo);
