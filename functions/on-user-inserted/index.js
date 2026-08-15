c8d893ca0f09eaf4ccaec839b56c0201411c6ad5
Anirban Paul <anirbanpaul@Anirbans-MacBook-Air.local>
2026-08-15 21:45:08 +0530

    Changes

--- functions/on-user-inserted/index.js
+++ functions/on-user-inserted/index.js
@@ -6,7 +6,7 @@ admin.initializeApp()
 console.log('firebase-admin version:', require('firebase-admin/package.json').version);
 console.log('admin keys:', Object.keys(admin));
 console.log('typeof admin.auth:', typeof admin.auth);
-console.log(brevo);
+// console.log(brevo);
 const apiInstance = new brevo.BrevoClient({apiKey : process.env.BREVO_API_KEY});
 
 async function sendVerificationEmail(toEmail, link) {
